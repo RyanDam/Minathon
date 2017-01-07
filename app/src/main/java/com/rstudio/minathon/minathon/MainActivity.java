031e75f08344d8814af56e80df8e5d2f97e7c164
RyanDam <py.ryan.dam@gmail.com>
2017-01-07 14:05:42 +0700

    Revert "Sum"
    This reverts commit 1aa0c3f646e54ee1f6cbbbc8f33ac1e3303a759b.

--- app/src/main/java/com/rstudio/minathon/minathon/MainActivity.java
+++ app/src/main/java/com/rstudio/minathon/minathon/MainActivity.java
@@ -2,47 +2,12 @@ package com.rstudio.minathon.minathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.rstudio.minathon.minathon.view.custom.CircleTime;
-import com.rstudio.minathon.minathon.view.custom.CircleTimeListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final CircleTime cl = ((CircleTime) findViewById(R.id.time));
-
-        Button bt = ((Button) findViewById(R.id.start));
-
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cl.startClock(65, new CircleTimeListener() {
-                    @Override
-                    public void onClockStart() {
-                        Log.d(TAG, "onClockStart: ");
-                    }
-
-                    @Override
-                    public void onClockEnd() {
-                        Log.d(TAG, "onClockEnd: ");
-                    }
-
-                    @Override
-                    public void onClockUpdate(float progress) {
-                        Log.d(TAG, "onClockUpdate: ");
-                    }
-                });
-            }
-        });
-
     }
 }
