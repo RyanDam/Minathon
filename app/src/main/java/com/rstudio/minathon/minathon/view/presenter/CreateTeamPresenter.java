752f364bb31b155bbd13b6b7ccc043c280a43b97
TienHaiHo <haiqhdk@gmail.com>
2017-01-07 14:13:37 +0700

    init

---
+++ app/src/main/java/com/rstudio/minathon/minathon/view/presenter/CreateTeamPresenter.java
@@ -0,0 +1,15 @@
+package com.rstudio.minathon.minathon.view.presenter;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by dkkbg_000 on 07/01/2017.
+ */
+
+public class CreateTeamPresenter {
+    public static final int DIALOG_DATE = 1;
+    public static final int DIALOG_TIME = 2;
+    public static SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm");
+    private String mTime;
+
+}
