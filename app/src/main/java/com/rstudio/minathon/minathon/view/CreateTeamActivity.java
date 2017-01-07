52bf0b166336abc33463dc11a73d7bb4ed2fc0fe
TienHaiHo <haiqhdk@gmail.com>
2017-01-07 10:16:59 +0700

    init

---
+++ app/src/main/java/com/rstudio/minathon/minathon/view/CreateTeamActivity.java
@@ -0,0 +1,15 @@
+package com.rstudio.minathon.minathon.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rstudio.minathon.minathon.R;
+
+public class CreateTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_team);
+    }
+}
---
+++ app/src/main/java/com/rstudio/minathon/minathon/view/JoinTeamActivity.java
@@ -0,0 +1,15 @@
+package com.rstudio.minathon.minathon.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rstudio.minathon.minathon.R;
+
+public class JoinTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_team);
+    }
+}
