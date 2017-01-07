6747b601ebce57e5251725d0a5ffb79d5eeb4388
TienHaiHo <haiqhdk@gmail.com>
2017-01-07 11:19:40 +0700

    init

--- app/src/main/java/com/rstudio/minathon/minathon/view/JoinTeamActivity.java
+++ app/src/main/java/com/rstudio/minathon/minathon/view/JoinTeamActivity.java
@@ -2,10 +2,20 @@ package com.rstudio.minathon.minathon.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.rstudio.minathon.minathon.R;
 
+import butterknife.BindView;
+
 public class JoinTeamActivity extends AppCompatActivity {
+    @BindView(R.id.edtTeamId)
+    EditText edtTeamId;
+    @BindView(R.id.edtUserName)
+    EditText edtUsername;
+    @BindView(R.id.btnJoinTeam)
+    Button btnJoinTeam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
